c9cb3aa7f175cb70bda28e08770c5368db7b3b7c
Franco Ortega <francopdx@gmail.com>
2020-11-05 11:23:05 -0800

    update componentDidMount, state, and create handleSumbit event listener

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import request from 'superagent';
 
+const userFromLocalStorage = {
+    userID: 1
+}
+
 export default class Create extends Component {
 
     state = {
@@ -9,29 +13,55 @@ export default class Create extends Component {
     }
 
     componentDidMount = async () => {
-        this.fetchPotion()
+        const response = await request.get(`https://agile-cove-58837.herokuapp.com/brands`);
+
+        this.setState({ brands: response.body});
     }
 
-    //https://agile-cove-58837.herokuapp.com/potions
-    fetchPotion = async () => {
-        this.setState({
-            loading: true
-        });
-        const response = await request.get(`https://agile-cove-58837.herokuapp.com/potions`);
 
-        this.setState({ potionData: response.body, loading: false})
+    // componentDidMount = async () => {
+    //     this.fetchPotion()
+    // }
+
+    // //https://agile-cove-58837.herokuapp.com/potions
+    // fetchPotion = async () => {
+    //     this.setState({
+    //         loading: true
+    //     });
+    //     const response = await request.get(`https://agile-cove-58837.herokuapp.com/potions`);
+
+    //     this.setState({ potionData: response.body, loading: false})
+    // }
+
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const newPotion = {
+            brand_id: this.state.brandId,
+            spell_level: this.state.spellLevel,
+            owner_id: this.state.userFromLocalStorage
+        }
     }
 
     render() {
         return (
-            <div>
-                CREATE PAGE
-                <form>
-                    <select>
-                        <option value='potion'>Potion</option>
-                        <option value='spellLevel'>Spell Level</option>
-                        <option value='brandId'>Brand</option>
-                    </select>
+            <div className='create-div'>
+                Create a potion.
+                <form className='form-div' onSumbit={this.handleSubmit}>
+                    <label>Potion: <input></input></label>
+                    <label>Spell Level: <input></input></label>
+                    <div>
+                        Brand: <select onChange={this.handleChange}>
+                            {
+                                this.state.brands.map(brand => <option key={brand.id} value={brand.id} />
+                            }
+                            <option value="Arkex Brews">Arkex Brews</option>
+                            <option value="Davan's Draughts">Davan's Draughts</option>
+                            <option value="Ismelda's Elixir's">Brand</option>
+                            <option value="Wild Tonics">Wild Tonics</option>
+                        </select>
+                    </div>
                     <button>
                         Create Potion
                     </button>
